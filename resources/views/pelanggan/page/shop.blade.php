2bb9545d23d4d10be7041df166a20f95a9b1e978
jalzae <jalzae@gmail.com>
2024-07-19 07:55:30 +0700

    multi checked type

--- resources/views/pelanggan/page/shop.blade.php
+++ resources/views/pelanggan/page/shop.blade.php
@@ -1,56 +1,48 @@
 @extends('pelanggan.layout.index')
 
 @section('content')
-    <div class="d-flex flex-row gap-2 mt-4">
-        <div class="" style="width: 30%;">
-            <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                    Kategory
-                </div>
-                <div class="card-body">
-                    <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingOne">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#flush-collapseOne" aria-expanded="false"
-                                    aria-controls="flush-collapseOne">
-                                    Jenis barang
-                                </button>
-                            </h2>
-                            <div id="flush-collapseOne" class="accordion-collapse collapse"
-                                aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">
-                                    <div class="d-flex flex-column gap-4">
-                                        <div class="d-flex flex-row gap-3">
-                                            <input type="checkbox" name="kategory" class="kategory" value="Helm half-face">
-                                            <span>Helm half-face</span>
-                                        </div>
-                                        <div class="d-flex flex-row gap-3">
-                                            <input type="checkbox" name="kategory" class="kategory" value="Helm full-face">
-                                            <span>Helm full-face</span>
-                                        </div>
+<div class="d-flex flex-row gap-2 mt-4">
+    <div class="" style="width: 30%;">
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                Kategory
+            </div>
+            <div class="card-body">
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                Jenis barang
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <div class="d-flex flex-column gap-4">
+                                    <div class="d-flex flex-row gap-3">
+                                        <input type="checkbox" name="kategory" class="kategory" value="Helm half-face">
+                                        <span>Helm half-face</span>
+                                    </div>
+                                    <div class="d-flex flex-row gap-3">
+                                        <input type="checkbox" name="kategory" class="kategory" value="Helm full-face">
+                                        <span>Helm full-face</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#flush-collapseTwo" aria-expanded="false"
-                                    aria-controls="flush-collapseTwo">
-                                    Accessories Helm
-                                </button>
-                            </h2>
-                            <div id="flush-collapseTwo" class="accordion-collapse collapse"
-                                aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body p-0">
-                                    <div class="accordion-body">
-                                        <div class="d-flex flex-column gap-4">
-                                            <div class="d-flex flex-row gap-3">
-                                                <input type="checkbox" name="kategory" class="kategory"
-                                                    value="Accessories">
-                                                <span>Accessories</span>
-                                            </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                Accessories Helm
+                            </button>
+                        </h2>
+                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body p-0">
+                                <div class="accordion-body">
+                                    <div class="d-flex flex-column gap-4">
+                                        <div class="d-flex flex-row gap-3">
+                                            <input type="checkbox" name="kategory" class="kategory" value="Accessories">
+                                            <span>Accessories</span>
                                         </div>
                                     </div>
                                 </div>
@@ -60,62 +52,67 @@
                 </div>
             </div>
         </div>
-        <div class="d-flex flex-wrap gap-4 mb-5" id="filterResult">
-            @if ($data->isEmpty())
-                <h1>Belum ada product ...!</h1>
-            @else
-                @foreach ($data as $p)
-                    <div class="card" style="width:200px;">
-                        <div class="card-header m-auto">
-                            <img src="{{ asset('storage/product/' . $p->foto) }}" alt="baju 1"
-                                style="width: 100%;height:130px; object-fit: cover; padding:0;">
-                        </div>
-                        <div class="card-body">
-                            <p class="m-0 text-justify" style="font-size: 14px;"> {{ $p->nama_product }} </p>
-                            <p class="m-0"><i class="fa-regular fa-star"></i> 5+</p>
-                        </div>
-                        <div class="card-footer d-flex flex-row justify-content-between align-items-center">
-                            <p class="m-0" style="font-size: 14px; font-weight:600;"><span>IDR
-                                </span>{{ number_format($p->harga) }}</p>
-                            <button class="btn btn-outline-primary" style="font-size:24px">
-                                <i class="fa-solid fa-cart-plus"></i>
-                            </button>
-                        </div>
-                    </div>
-                @endforeach
-        </div>
     </div>
-    <div class="pagination d-flex flex-row justify-content-between">
-        <div class="showData">
-            Data ditampilkan {{ $data->count() }} dari {{ $data->total() }}
+    <div class="d-flex flex-wrap gap-4 mb-5" id="filterResult">
+        @if ($data->isEmpty())
+        <h1>Belum ada product ...!</h1>
+        @else
+        @foreach ($data as $p)
+        <div class="card" style="width:200px;">
+            <div class="card-header m-auto">
+                <img src="{{ asset('storage/product/' . $p->foto) }}" alt="baju 1" style="width: 100%;height:130px; object-fit: cover; padding:0;">
+            </div>
+            <div class="card-body">
+                <p class="m-0 text-justify" style="font-size: 14px;"> {{ $p->nama_product }} </p>
+                <p class="m-0"><i class="fa-regular fa-star"></i> 5+</p>
+            </div>
+            <div class="card-footer d-flex flex-row justify-content-between align-items-center">
+                <p class="m-0" style="font-size: 14px; font-weight:600;"><span>IDR
+                    </span>{{ number_format($p->harga) }}</p>
+                <button class="btn btn-outline-primary" style="font-size:24px">
+                    <i class="fa-solid fa-cart-plus"></i>
+                </button>
+            </div>
         </div>
-        <div>
-            {{ $data->links() }}
-        </div>
+        @endforeach
     </div>
-    @endif
+</div>
+<div class="pagination d-flex flex-row justify-content-between">
+    <div class="showData">
+        Data ditampilkan {{ $data->count() }} dari {{ $data->total() }}
+    </div>
+    <div>
+        {{ $data->links() }}
+    </div>
+</div>
+@endif
 
-    <script>
-        $(document).ready(function() {
-            $('.kategory').change(function(e) {
-                e.preventDefault();
-                var value = $(this).val();
-                var split = value.split(' ');
-                var kategory = split[0];
-                var type = split[1];
-                // alert(type);
-                $.ajax({
-                    type: "GET",
-                    url: "{{ route('shop') }}",
-                    data: {
-                        kategory: kategory,
-                        type: type,
-                    },
-                    success: function(response) {
-                        console.log(response);
-                    }
-                });
-            });
+<script>
+    $(document).ready(function() {
+        $('.kategory').change(function(e) {
+            e.preventDefault();
+            
+            const checkboxes = document.querySelectorAll('.kategory');
+            let checkedValues = [];
+
+            checkedValues = Array.from(checkboxes)
+                .filter(i => i.checked)
+                .map(i => i.value);
+            console.log(checkedValues);
+            // alert(type);
+            //$.ajax({
+            //    type: "GET",
+            //    url: "{{ route('shop') }}",
+            //    data: {
+            //         kategory: kategory,
+            //        type: type,
+            //    },
+            //    success: function(response) {
+            //        console.log(response);
+            //   }
+            //});
+
         });
-    </script>
+    });
+</script>
 @endsection
